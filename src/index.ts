13c972c571bed72b7af2c0f54d09e976215f00aa
finoru <ruffinoahmadnoor@gmail.com>
2025-09-30 01:27:33 +0700

    add server checking endpoint

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,15 @@ async function init() {
 
     app.use(bodyParser.json());
 
+    app.get("/", (req, res) => {
+      return res
+        .status(200)
+        .json({
+          message: "Server is running!, bismillah fullstack",
+          data: null,
+        });
+    });
+
     app.use("/api", router);
 
     app.listen(PORT, () => {
