b8dfe26b34df923ad86eff240dd2408d7aaec13a
finoru <ruffinoahmadnoor@gmail.com>
2025-10-08 04:01:06 +0700

    feat: add api documentation for login and register

---
+++ src/docs/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API ACARA",
+    description: "Dokumentasi API ACARA",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Server",
+    },
+    {
+      url: "https://back-end-penjualan-tiket-acara.vercel.app/",
+      description: "Deploy Server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "fino1908",
+        password: "123456789",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import router from "./routes/api";
 
 import db from "./utils/database";
+import docs from "./docs/route";
 
 async function init() {
   try {
@@ -14,18 +16,18 @@ async function init() {
 
     const PORT = 3000;
 
+    app.use(cors());
     app.use(bodyParser.json());
 
     app.get("/", (req, res) => {
-      return res
-        .status(200)
-        .json({
-          message: "Server is running!, bismillah fullstack",
-          data: null,
-        });
+      return res.status(200).json({
+        message: "Server is running!, bismillah fullstack",
+        data: null,
+      });
     });
 
     app.use("/api", router);
+    docs(app);
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
